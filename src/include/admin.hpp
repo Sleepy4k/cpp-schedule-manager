db178e8937b6ecf7dc830e64575960f8fb5d5715
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-27 00:53:33 +0700

    chore: some include

--- src/include/admin.hpp
+++ src/include/admin.hpp
@@ -6,12 +6,21 @@ using namespace std;
 class Admin {
   private:
     struct ScheduleStruct {
+      string uid;
       string date;
       string subject;
       string faculty;
-    }; 
+    };
+    
+    bool is_data_numeric(string data);
+    bool validate_date(string date);
+    bool validate_string(string text);
+    bool validate_uid(string text);
   public:
     void show_menu();
     void show_schedule();
     void add_schedule();
+    void find_schedule();
+    void edit_schedule();
+    void delete_schedule();
 };
--- src/include/handler.hpp
+++ src/include/handler.hpp
@@ -1,13 +1,18 @@
+#include <string>
+
 using namespace std;
 
 class Handler {
   private:
     struct UserRecord {
+      string uid;
       string username;
       string password;
       string role;
     };
   public:
     int main_menu();
+    string generate_uuid();
     void handle_login(string role);
+    void handle_register();
 };
