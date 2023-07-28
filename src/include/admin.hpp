bae97ad33103b3803162b8b2a1658803e91d5d1f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-28 18:41:44 +0700

    chore: some include

--- src/include/admin.hpp
+++ src/include/admin.hpp
@@ -4,19 +4,14 @@
 using namespace std;
 
 class Admin {
-  private:
+  public:
     struct ScheduleStruct {
       string uid;
       string date;
       string subject;
       string faculty;
     };
-    
-    bool is_data_numeric(string data);
-    bool validate_date(string date);
-    bool validate_string(string text);
-    bool validate_uid(string text);
-  public:
+
     void show_menu();
     void show_schedule();
     void add_schedule();
