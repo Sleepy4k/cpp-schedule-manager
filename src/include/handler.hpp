b9e291e45e7506037902d3cccde26850f9c3495e
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-07-30 18:01:57 +0700

    chore: handler include

--- src/include/handler.hpp
+++ src/include/handler.hpp
@@ -15,6 +15,8 @@ class Handler {
     string generate_uuid();
     string handle_login(string role);
     void handle_register();
+    string input_validation(string data, string type, bool is_ws);
+    int menu_validation(int min, int max);
     bool is_data_numeric(string data);
     bool validate_date(string date);
     bool validate_string(string text);
